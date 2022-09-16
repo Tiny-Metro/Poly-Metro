520892485039cfd42164fb4ca5775566547a6aac
JaeWon Lee <ljw2889@gmail.com>
2022-09-16 16:37:18 +0900

    [Init] Project

---
+++ Source/TinyMetro/Private/Sample/SampleEmptyClass.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Sample/SampleEmptyClass.h"
+
+SampleEmptyClass::SampleEmptyClass()
+{
+}
+
+SampleEmptyClass::~SampleEmptyClass()
+{
+}
+
+int SampleEmptyClass::getNumber(int num)
+{
+	return num;
+}
