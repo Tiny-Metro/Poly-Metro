3f58e25028be79a5ed92b8214b00d3e0773afeb0
JaeWon Lee <ljw2889@gmail.com>
2022-12-06 22:27:53 +0900

    Revert "Merge pull request #45 from Tiny-Metro/develop-camera"
    This reverts commit e1cc6e5fd42bab1ade9c00d786d5fbfd6b8a3874, reversing
    changes made to 6af67426fc5902b1688593555bc784a03eaaa00b.

--- Source/TinyMetro/Private/TextReader.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "TextReader.h"
-
-bool UTextReader::LoadMapText(FString FileName, FString& OutText) {
-	return FFileHelper::LoadFileToString(OutText, *(FPaths::ProjectContentDir() + "Resource/Mapdata/" + FileName));
-}
