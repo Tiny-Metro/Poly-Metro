a83769bc53f4e1a7f8e4bce1ec0cb52dd4ee4ea6
JaeWon Lee <ljw2889@gmail.com>
2022-12-09 16:46:25 +0900

    [Update] Build error fix.. etc

--- Source/TinyMetro/Private/Station/Passenger.cpp
+++ Source/TinyMetro/Private/Station/Passenger.cpp
@@ -6,3 +6,9 @@
 void UPassenger::SetDestination(StationType Dest) {
 	Destination = Dest;
 }
+
+UPassenger* UPassenger::ConstructPassenger(StationType Dest) {
+	auto Obj = NewObject<UPassenger>();
+	Obj->SetDestination(Dest);
+	return Obj;
+}
