9b71ba3967c77f991eee4f400ac6922952f72d94
JaeWon Lee <ljw2889@gmail.com>
2022-12-21 11:01:45 +0900

    [Add] Shop class (Actor)

---
+++ Source/TinyMetro/Private/Shop/Shop.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Shop/Shop.h"
+
+// Sets default values
+AShop::AShop()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AShop::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShop::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
