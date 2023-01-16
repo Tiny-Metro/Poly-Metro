3a3fcf1fef97a1e83e776edbe2a17551f0ea353e
JaeWon Lee <ljw2889@gmail.com>
2023-01-16 20:56:59 +0900

    [Add] Subtrain class

---
+++ Source/TinyMetro/Public/Train/Subtrain.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Train/TrainTemplate.h"
+#include "Subtrain.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINYMETRO_API ASubtrain : public ATrainTemplate
+{
+	GENERATED_BODY()
+	
+};
