560355ac508d53bf9db4f09421ac06e5954d113c
JaeWon Lee <ljw2889@gmail.com>
2023-02-16 18:57:00 +0900

    [Update] Add commit on 3a398ec101b96ba49474fb6bb59cc1da9a16894f

--- Source/TinyMetro/Public/HUD/SpawnTrainTemplate.h
+++ Source/TinyMetro/Public/HUD/SpawnTrainTemplate.h
@@ -14,11 +14,25 @@ class TINYMETRO_API USpawnTrainTemplate : public UUserWidget
 {
 	GENERATED_BODY()
 
+public:
+	//virtual void Construct() override;
+	virtual void NativeConstruct() override;
+
 protected:
 	UFUNCTION(BlueprintCallable)
 	UPARAM(DisplayName = "Hit Actor") AActor* ConvertMousePositionToWorldLocation(FVector& WorldLocation);
 	
+
 protected:
 	UPROPERTY(BlueprintReadWrite)
 	TArray<AActor*> LineTraceIgnoreActors;
+	UPROPERTY(BlueprintReadOnly)
+	class AGridManager* GridManagerRef;
+	UPROPERTY(BlueprintReadOnly)
+	class AStationManager* StationManagerRef;
+	UPROPERTY(BlueprintReadOnly)
+	class ATrainManager* TrainManagerRef;
+	UPROPERTY(BlueprintReadOnly)
+	class ALaneManager* LaneManagerRef;
+
 };
