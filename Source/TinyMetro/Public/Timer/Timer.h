22965fa766654a7da52e9ed2ca29972138e159df
Sol Lee <ljw2889@gmail.com>
2023-05-03 18:17:45 +0900

    [Update] Timer routine optimize

--- Source/TinyMetro/Public/Timer/Timer.h
+++ Source/TinyMetro/Public/Timer/Timer.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Actor.h"
 #include "Timer.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDailyTask);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWeeklyTask);
+
 UCLASS()
 class TINYMETRO_API ATimer : public AActor
 {
@@ -26,6 +29,10 @@ public:
 public:
 	UPROPERTY()
 	float ElapseTimeSec;
+	UPROPERTY(BlueprintAssignable)
+	FDailyTask DailyTask;
+	UPROPERTY(BlueprintAssignable)
+	FWeeklyTask WeeklyTask;
 
 protected:
 	UPROPERTY()
