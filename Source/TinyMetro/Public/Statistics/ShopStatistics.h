c9303796c0d262d803df899219531015317ca530
Sol Lee <ljw2889@gmail.com>
2023-07-19 18:48:33 +0900

    [Add] Statistics classes

---
+++ Source/TinyMetro/Public/Statistics/ShopStatistics.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ShopStatistics.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINYMETRO_API UShopStatistics : public UObject
+{
+	GENERATED_BODY()
+	
+};
