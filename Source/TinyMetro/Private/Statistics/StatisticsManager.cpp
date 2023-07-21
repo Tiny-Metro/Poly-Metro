7cfe6275640ab53febd8784e793800485c2a5696
Sol Lee <ljw2889@gmail.com>
2023-07-21 16:41:42 +0900

    [Update] Add default functions

--- Source/TinyMetro/Private/Statistics/StatisticsManager.cpp
+++ Source/TinyMetro/Private/Statistics/StatisticsManager.cpp
@@ -2,6 +2,10 @@
 
 
 #include "Statistics/StatisticsManager.h"
+#include "GameModes/TinyMetroGameModeBase.h"
+#include "Timer/Timer.h"
+#include "SaveSystem/TMSaveManager.h"
+#include <Kismet/GameplayStatics.h>
 
 // Sets default values
 AStatisticsManager::AStatisticsManager()
@@ -15,13 +19,41 @@ AStatisticsManager::AStatisticsManager()
 void AStatisticsManager::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// Get outer actors references
+	if (GameModeRef == nullptr) GameModeRef = Cast<ATinyMetroGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+	if (TimerRef == nullptr) TimerRef = GameModeRef->GetTimer();
+	if (SaveManagerRef == nullptr) SaveManagerRef = GameModeRef->GetSaveManager();
 	
+
+	// Bind daily, weekly tasks
+	TimerRef->DailyTask.AddDynamic(this, &AStatisticsManager::DailyTask);
+	TimerRef->WeeklyTask.AddDynamic(this, &AStatisticsManager::WeeklyTask);
+
+	// Bind save task
+	SaveManagerRef->SaveTask.AddDynamic(this, &AStatisticsManager::Save);
 }
 
 // Called every frame
 void AStatisticsManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+}
+
+void AStatisticsManager::DailyTask() {
+}
+
+void AStatisticsManager::WeeklyTask() {
+}
+
+void AStatisticsManager::Save() {
+}
+
+void AStatisticsManager::Load() {
+	// Get outer actors references
+	if (GameModeRef == nullptr) GameModeRef = Cast<ATinyMetroGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+	if (SaveManagerRef == nullptr) SaveManagerRef = GameModeRef->GetSaveManager();
 
 }
 
