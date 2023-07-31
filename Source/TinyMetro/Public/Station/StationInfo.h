e04f8e2f47b2fd6a5ea412a6a7185f1d99b8131a
Sol Lee <ljw2889@gmail.com>
2023-07-31 16:40:05 +0900

    [Add] Station info

--- Source/TinyMetro/Public/Station/StationInfo.h
+++ Source/TinyMetro/Public/Station/StationInfo.h
@@ -19,9 +19,27 @@ public:
 
 	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
 	int32 Id;
-
 	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
 	StationType Type;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+	int32 ServiceLaneCount;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+	int32 TotalUsingPassenger;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+	int32 WeeklyUsingPassenger;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+	int32 TotalProfit;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+	int32 WeeklyProfit;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+	float Complain;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+	float AverageWaitTime;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+	int32 TotalTransferPassenger;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+	int32 WeeklyTransferPassenger;
+
 
 	FStationInfo() { Id = 0; Type = StationType::Circle; }
 
