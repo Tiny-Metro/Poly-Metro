8a13ac31b0b6273dd2ae1225de3df8b53dcac585
김경준 <dmb07250@gmail.com>
2023-08-16 17:19:16 +0900

    [Update] convert Rtscamera to C++

--- Source/TinyMetro/Private/Camera/RtsCamera.cpp
+++ Source/TinyMetro/Private/Camera/RtsCamera.cpp
@@ -1,34 +1,144 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Camera/RtsCamera.h"
+#include "Engine/Classes/Components/SphereComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
 
-// Sets default values
-ARtsCamera::ARtsCamera()
+
+ARtsCamera::ARtsCamera (const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+
+	CustomCollisionComponent = ObjectInitializer.CreateDefaultSubobject<USphereComponent>(this, FName("RtsCamera_CollisionComponent0"));
+	CustomCollisionComponent->InitSphereRadius(100.0f);
+	CustomCollisionComponent->CanCharacterStepUpOn = ECB_No;
+	CustomCollisionComponent->SetCanEverAffectNavigation(false);
+	CustomCollisionComponent->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	RootComponent = CustomCollisionComponent;
+
+	MovementComponent = ObjectInitializer.CreateDefaultSubobject<UFloatingPawnMovement>(this, FName("RtsCameran_MovementComponent0"));
+	MovementComponent->UpdatedComponent = CustomCollisionComponent;
+}
+
+void ARtsCamera::SetupPlayerInputComponent(UInputComponent* Inputelemental)
+{
+	check(Inputelemental);
+
+	Inputelemental->BindAxis("Pan Forward", this, &ARtsCamera::PanForward);
+	Inputelemental->BindAxis("Pan Right", this, &ARtsCamera::PanRight);
+	Inputelemental->BindAxis("Mouse Wheel", this, &ARtsCamera::ZoomIn);
+
+	Inputelemental->BindAction("Rotate Camera", IE_Pressed, this, &ARtsCamera::EngageRotation);
+	Inputelemental->BindAction("Rotate Camera", IE_Released, this, &ARtsCamera::DisengageRotation);
+}
+
+void ARtsCamera::Tick(float DeltaSeconds)
+{
+	if (Controller)
+	{
+		APlayerController* playerController = Cast<APlayerController>(GetController());
+		if (playerController != nullptr)
+		{
+			FVector2D mouseLocation;
+			if (playerController->GetMousePosition(mouseLocation.X, mouseLocation.Y))
+			{
+				int32 screenWidth = 0;
+				int32 screenHeight = 1000;
+				playerController->GetViewportSize(screenWidth, screenHeight);
+
+				if (rotationMode) //Main Camera rotation calls
+				{
+					Orbit((mouseScreenLocationGrab.X - mouseLocation.X) / screenWidth);
+					ChangePitch((mouseScreenLocationGrab.Y - mouseLocation.Y) * -1 / screenWidth);
+				}
+				else //  Mouse on screen edge pan
+				{
+					const float xPanZone = screenWidth * ScreenEdgePanZonePercent / 100;
+					const float yPanZone = screenHeight * ScreenEdgePanZonePercent / 100;
+
+					if (mouseLocation.X < xPanZone)							PanRight((1 - mouseLocation.X / xPanZone) * -1);
+					else if (mouseLocation.X > screenWidth - xPanZone)		PanRight(1 - (screenWidth - mouseLocation.X) / xPanZone);
+					if (mouseLocation.Y < yPanZone)							PanForward(1 - mouseLocation.Y / yPanZone);
+					else if (mouseLocation.Y > screenHeight - yPanZone)		PanForward((1 - (screenHeight - mouseLocation.Y) / yPanZone) * -1);
+
+				}
+
+
+			}
+		}
+	}
+}
+
+void ARtsCamera::ChangePitch(const float magnitude)
+{
+	if (magnitude != 0 && rotationMode && Controller)
+		if (magnitude < 0 && GetControlRotation().Pitch < MaxPitch || magnitude > 0 && GetControlRotation().Pitch > MinPitch)
+			AddControllerPitchInput(magnitude * PitchChangeRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ARtsCamera::Yaw(const float magnitude)
+{
+	if (magnitude != 0 && rotationMode && Controller)
+	{
+		AddControllerYawInput(magnitude * YawRate * GetWorld()->GetDeltaSeconds());
+	}
 
 }
 
-// Called when the game starts or when spawned
-void ARtsCamera::BeginPlay()
+void ARtsCamera::Orbit(const float magnitude)
 {
-	Super::BeginPlay();
-	
+	if (magnitude != 0 && rotationMode && Controller)
+	{
+		APlayerController* playerController = Cast<APlayerController>(GetController());
+		if (playerController != nullptr)
+		{
+			AddControllerYawInput(magnitude * YawRate * GetWorld()->GetDeltaSeconds());
+			AddMovementInput(FRotationMatrix(GetControlRotation()).GetScaledAxis(EAxis::Y), magnitude * -(YawRate / 4) * GetWorld()->GetDeltaSeconds());
+		}
+	}
+
 }
 
-// Called every frame
-void ARtsCamera::Tick(float DeltaTime)
+void ARtsCamera::PanForward(const float magnitude)
 {
-	Super::Tick(DeltaTime);
-
+	if (magnitude != 0 && !rotationMode && Controller)
+		AddMovementInput(FRotationMatrix(FRotator(0, GetControlRotation().Yaw, 0)).GetScaledAxis(EAxis::X), magnitude * 4000 * GetWorld()->GetDeltaSeconds());
 }
 
-// Called to bind functionality to input
-void ARtsCamera::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ARtsCamera::PanRight(const float magnitude)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	if (magnitude != 0 && !rotationMode && Controller)
+		AddMovementInput(FRotationMatrix(GetControlRotation()).GetScaledAxis(EAxis::Y), magnitude * PanRate * GetWorld()->GetDeltaSeconds());
 }
+
+void ARtsCamera::ZoomIn(const float magnitude)
+{
+	if (magnitude != 0 && !rotationMode && Controller)
+		if (magnitude > 0 && GetActorLocation().Z > MinZoom || magnitude < 0 && GetActorLocation().Z < MaxZoom)
+			AddMovementInput(FRotationMatrix(GetControlRotation()).GetScaledAxis(EAxis::X), magnitude * ZoomRate * GetWorld()->GetDeltaSeconds());
+}
+
+
+
+
+void ARtsCamera::EngageRotation()
+{
+	if (Controller)
+	{
+		APlayerController* playerController = Cast<APlayerController>(GetController());
+		if (playerController != nullptr)
+		{
+			FVector2D mousePosition;
+			if (playerController->GetMousePosition(mousePosition.X, mousePosition.Y))
+			{
+				mouseScreenLocationGrab = mousePosition;
+				rotationMode = true;
+			}
+		}
+	}
+}
+
+void ARtsCamera::DisengageRotation()
+{
+	rotationMode = false;
+}
+
+
 
--- Source/TinyMetro/Public/Camera/RtsController.h
+++ Source/TinyMetro/Public/Camera/RtsController.h
@@ -1,60 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
-#include "CoreMinimal.h"
-#include "GameFramework/PlayerController.h"
+#include "RtsCamera.h"
+#include "GameFramework/Pawn.h"
 #include "RtsController.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class TINYMETRO_API ARtsController : public APlayerController
 {
 	GENERATED_BODY()
-	
-public:
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
-	void AutoCameraZoomIn(FVector Location);
-	virtual void AutoCameraZoomIn_Implementation(FVector Location);
-		
-	
-public:
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
-	void AutoCameraZoomOut(FVector Location);
-	virtual void AutoCameraZoomOut_Implementation(FVector Location);
 
 public:
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
-	void GameOverZoomIn(FVector Location);
-	virtual void GameOverZoomIn_Implementation(FVector Location);
+	ARtsController(const FObjectInitializer& ObjectInitializer);
 
 protected:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float CameraScrollSpeed = 1500.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float CameraScrollEdgeThreshhold = 100.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float CameraZoomSpeed = 100.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float CameraRotationSpeed = 200.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float GameoverZoomSpeed = 100.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float DelayZoom = 30.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float XLeftLimit = -500.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float XRightLimit = 500.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float YDownLimit = 500.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float YUpLimit = -500.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float DelayZoomOut = 30.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FRotator DefaultRotation = FRotator(0.0f, 0.0f, 0.0f);
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float GameOverZoomInSpeed = 30.0f;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
 };
