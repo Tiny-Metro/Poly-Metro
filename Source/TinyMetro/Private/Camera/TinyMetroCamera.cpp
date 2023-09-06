4ce1df9786eba2a7cbd690239084276f5c4904e0
Sol Lee <ljw2889@gmail.com>
2023-09-06 17:34:31 +0900

    [Add] Camera zoom, move

--- Source/TinyMetro/Private/Camera/TinyMetroCamera.cpp
+++ Source/TinyMetro/Private/Camera/TinyMetroCamera.cpp
@@ -25,7 +25,14 @@ ATinyMetroCamera::ATinyMetroCamera()
 void ATinyMetroCamera::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Cyan, TEXT("Pawn::BeginPlay"));
+	if (!IsValid(PlayerControllerRef)) PlayerControllerRef = Cast<APlayerController>(Controller);
+	PlayerControllerRef->GetViewportSize(ScreenX, ScreenY);
+
+	RootComponent->SetWorldRotation(FRotator(0.0f, -90.0f, 0.0f));
+
+	SpringArmComponenet->SetRelativeRotation(FRotator(-60.0f, 0.0f, 0.0f));
+	SpringArmComponenet->TargetArmLength = ZoomScale;
 }
 
 void ATinyMetroCamera::MouseWheel(float Axis) {
@@ -45,11 +52,68 @@ void ATinyMetroCamera::MouseWheel(float Axis) {
 	}
 }
 
+void ATinyMetroCamera::MouseX(float Axis) {
+	if(Axis != 0.0f) UE_LOG(LogTemp, Log, TEXT("MouseX : %f"), Axis);
+	if (IsCameraRotation) {
+		CameraComponent->AddLocalRotation(FRotator(0.0f, Axis, 0.0f).Quaternion());
+		SpringArmComponenet->AddLocalRotation(FRotator(Axis, 0.0f, 0.0f).Quaternion());
+	}
+}
+
+void ATinyMetroCamera::MouseY(float Axis) {
+	if (Axis != 0.0f) UE_LOG(LogTemp, Log, TEXT("MouseY : %f"), Axis);
+	if (IsCameraRotation) {
+		SpringArmComponenet->AddLocalRotation(FRotator(Axis, 0.0f, 0.0f).Quaternion());
+		//CameraComponent->AddLocalRotation(FRotator(Axis, 0.0f, 0.0f).Quaternion());
+	}
+}
+
+void ATinyMetroCamera::CameraRotationOn() {
+	IsCameraRotation = true;
+}
+
+void ATinyMetroCamera::CameraRotationOff() {
+	IsCameraRotation = false;
+}
+
+void ATinyMetroCamera::CameraMoveX(int32 DirectionFlag, float DeltaTime) {
+	// DirectionFlag > 0 : Move Right
+	// DirectionFlag < 0 : Move Left
+	if (CameraMoveEnable) {
+		auto currentLocation = GetActorLocation();
+		currentLocation.X += DirectionFlag * CameraMoveSpeedX * DeltaTime;
+		SetActorLocation(currentLocation);
+	}
+}
+
+void ATinyMetroCamera::CameraMoveY(int32 DirectionFlag, float DeltaTime) {
+	// DirectionFlag > 0 : Move Down
+	// DirectionFlag < 0 : Move Up
+	if (CameraMoveEnable) {
+		auto currentLocation = GetActorLocation();
+		currentLocation.Y += DirectionFlag * CameraMoveSpeedX * DeltaTime;
+		SetActorLocation(currentLocation);
+	}
+}
+
+void ATinyMetroCamera::SetCameraMoveEnable(bool Flag) {
+	CameraMoveEnable = Flag;
+}
+
+void ATinyMetroCamera::PinchZoom(float Value) {
+	if (Value != 0.0f) {
+		SpringArmComponenet->TargetArmLength *= Value;
+	}
+}
+
 // Called every frame
 void ATinyMetroCamera::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	//PlayerControllerRef->GetMousePosition(MousePosition.X, MousePosition.Y);
+	//UE_LOG(LogTemp, Log, TEXT("X : %f / %d, Y : %f / %d"), MousePosition.X, ScreenX, MousePosition.Y, ScreenY);
 
+	
 }
 
 // Called to bind functionality to input
@@ -57,7 +121,16 @@ void ATinyMetroCamera::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAction(TEXT("Right Mouse Button"), IE_Pressed, this, &ATinyMetroCamera::CameraRotationOn);
+	PlayerInputComponent->BindAction(TEXT("Right Mouse Button"), IE_Released, this, &ATinyMetroCamera::CameraRotationOff);
+
+	// Bind wheel mapping (Camera zoom in, out)
 	PlayerInputComponent->BindAxis(TEXT("Mouse Wheel"), this, &ATinyMetroCamera::MouseWheel);
+	// Bind wheel mapping (Camera zoom in, out)
+	PlayerInputComponent->BindAxis(TEXT("Pinch Zoom"), this, &ATinyMetroCamera::PinchZoom);
+
+	PlayerInputComponent->BindAxis(TEXT("Mouse X"), this, &ATinyMetroCamera::MouseX);
+	PlayerInputComponent->BindAxis(TEXT("Mouse Y"), this, &ATinyMetroCamera::MouseY);
 
 }
 
--- Source/TinyMetro/Public/Camera/TinyMetroCamera.h
+++ Source/TinyMetro/Public/Camera/TinyMetroCamera.h
@@ -22,6 +22,27 @@ protected:
 	UFUNCTION()
 	void MouseWheel(float Axis);
 
+	UFUNCTION()
+	void MouseX(float Axis);
+	UFUNCTION()
+	void MouseY(float Axis);
+
+	UFUNCTION()
+	void CameraRotationOn();
+	UFUNCTION()
+	void CameraRotationOff();
+
+	UFUNCTION()
+	void CameraMoveX(int32 DirectionFlag, float DeltaTime);
+	UFUNCTION()
+	void CameraMoveY(int32 DirectionFlag, float DeltaTime);
+
+	UFUNCTION()
+	void SetCameraMoveEnable(bool Flag);
+
+	UFUNCTION()
+	void PinchZoom(float Value);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -31,12 +52,37 @@ public:
 
 protected:
 
+	UPROPERTY()
+	APlayerController* PlayerControllerRef;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	FVector2D MousePosition;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Move")
+	int32 ScreenX;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Move")
+	int32 ScreenY;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Move")
+	bool CameraMoveEnable = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Move")
+	float CameraMoveSpeedX = 20.0f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Move")
+	float CameraMoveSpeedY = 20.0f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Move")
+	float CameraMoveBoundary = 0.2f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Location")
+	FVector InitLocation = FVector(0, 3500.0f, 6000.f);
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Zoom")
 	float MinZoom = 300.0f;
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Zoom")
 	float MaxZoom = 3000.0f;
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Zoom")
 	float ZoomSpeed = 20.0f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Zoom")
+	float ZoomScale = 2000.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Rotation")
+	bool IsCameraRotation = false;
 
 	UPROPERTY(VisibleAnyWhere, BlueprintReadWrite)
 	class USpringArmComponent* SpringArmComponenet;
