b2d0f57156a5bbbce4707a9c618acfe70311b1a8
Sol Lee <ljw2889@gmail.com>
2023-10-09 14:16:28 +0900

    [Add] PlayerState save class

---
+++ Source/TinyMetro/Public/PlayerState/TinyMetroPlayerStateSaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "TinyMetroPlayerStateSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINYMETRO_API UTinyMetroPlayerStateSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
