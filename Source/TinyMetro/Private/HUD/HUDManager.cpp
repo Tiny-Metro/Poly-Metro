fa1e28f16d5d361adec6dcef0b538b828c6d6d61
KimDa99 <hyhy61451@gmail.com>
2023-10-12 19:04:53 +0900

    [Add] HUD related Class to Manage Size and other

---
+++ Source/TinyMetro/Private/HUD/HUDManager.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HUD/HUDManager.h"
+
+#include "AssetRegistryModule.h"
+
+
+AHUDManager::AHUDManager()
+{
+	CurrentLanguage = ELanguage::Korean;
+}
+
+void AHUDManager::ChangeLanguage(ELanguage NewLanguage)
+{
+    CurrentLanguage = NewLanguage;
+    // Update your HUD widgets to reflect the new language
+    // You can implement this based on your HUD widget design
+}
+
+void AHUDManager::SetImageTable()
+{
+//    ImageTable.Add("ToMenu_ExitIcon", LoadTextureFromFile(""));
+}
+
+FString AHUDManager::GetText(FString BigSection, FString SmallSection)
+{
+    if (StringTable.Contains(BigSection))
+    {
+        return StringTable[BigSection];
+    }
+    return FString(); // Return an empty string if the section is not found
+}
+
+void AHUDManager::AssignWidget(FString Name, UPolyMetroWidget* Widget)
+{
+    Widgets.Add(Name, Widget);
+}
+
+UPolyMetroWidget* AHUDManager::GetWidget(FString Name)
+{
+    return *Widgets.Find(Name);
+}
+
+FString AHUDManager::GetTextByName(FString Name)
+{
+    return *StringTable.Find(Name);
+}
+
+UTexture* AHUDManager::GetImageByName(FString Name)
+{
+    return *ImageTable.Find(Name);
+}
+
+UTexture* AHUDManager::LoadTextureFromFile(const FString& TexturePath)
+{
+    // Use the AssetRegistryModule to load the texture by path
+    UTexture* LoadedTexture = nullptr;
+
+    FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
+
+    TArray<FAssetData> AssetData;
+    AssetRegistryModule.Get().GetAssetsByPath(*TexturePath, AssetData);
+
+    if (AssetData.Num() > 0)
+    {
+        // You can load the first asset found
+        FStringAssetReference AssetRef(AssetData[0].ObjectPath.ToString());
+        LoadedTexture = Cast<UTexture>(AssetRef.TryLoad());
+    }
+
+    return LoadedTexture;
+}
---
+++ Source/TinyMetro/Public/HUD/HUDManager.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "Engine/Texture.h"
+#include "PolyMetroWidget.h"
+#include "Blueprint/UserWidget.h"
+
+#include "HUDManager.generated.h"
+
+UENUM(BlueprintType)
+enum class ELanguage : uint8
+{
+    English,
+    Korean
+};
+
+UENUM(BlueprintType)
+enum class ETextSize : uint8 
+{
+    S,
+    M,
+    L
+
+};
+
+UCLASS(Blueprintable)
+class TINYMETRO_API AHUDManager : public AHUD
+{
+    GENERATED_BODY()
+
+public:
+    AHUDManager();
+
+    UFUNCTION(BlueprintCallable, Category = "Localization")
+    void ChangeLanguage(ELanguage NewLanguage);
+
+    UFUNCTION(BlueprintCallable, Category = "Localization")
+    FString GetText(FString BigSection, FString SmallSection);
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Localization")
+    TMap<FString, FString> StringTable;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Localization")
+    TMap<FString, UTexture*> ImageTable;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Localization")
+    ELanguage CurrentLanguage;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Localization")
+    ETextSize CurrentTextSize;
+
+public:
+    //Widgets - Maps with string
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TMap<FString, UPolyMetroWidget*> Widgets;
+
+    UFUNCTION(BlueprintCallable)
+    void AssignWidget(FString Name, UPolyMetroWidget* Widget);
+
+    UFUNCTION(BlueprintCallable)
+    UPolyMetroWidget* GetWidget(FString Name);
+
+    UFUNCTION(BlueprintCallable)
+    FString GetTextByName(FString Name);
+
+    UFUNCTION(BlueprintCallable)
+    UTexture* GetImageByName(FString Name);
+
+private:
+    void SetImageTable();
+
+    UTexture* LoadTextureFromFile(const FString& TexturePath);
+};
